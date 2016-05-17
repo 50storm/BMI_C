4451a8ae6cb6efbe1b2806d2da49e2c5bc11354c
五十嵐博司 <hiroshi@hiroshi-mac.local>
2016-05-18 08:35:14 +0900

    Initial Commit

---
+++ BMI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BMI
+//
+//  Created by 五十嵐博司 on 2016/05/18.
+//  Copyright © 2016年 五十嵐博司. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
