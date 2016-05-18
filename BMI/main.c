f2ebe118bbd80828b9d89929ab60055b125b7110
五十嵐博司 <hiroshi@hiroshi-mac.local>
2016-05-19 07:42:14 +0900

    first commit

--- BMI/main.c
+++ BMI/main.c
@@ -7,9 +7,54 @@
 //
 
 #include <stdio.h>
+#define true 1
+#define false 0
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int fat, thin, normal;
+    double h,w,bmi;
+    
+    int flg_h=1;
+    while (flg_h) {
+        printf("Please input your hight (cm):");
+        scanf("%lf",&h );
+        
+        if(h<=0){
+            printf("Wrong input for height(cm):");
+            flg_h=1;
+            
+        }else{
+            flg_h=0;
+            
+        }
+        
+    }
+    
+    return 0;
+    
+    while (is_weghit_ok == 0) {
+        printf("Please input your weight (cm):");
+        scanf("%lf",&w );
+        if(w <= 0) {
+        }else{
+        }
+        
+   } 
+    
+    
+    
+    bmi = w*10000/(h*h);
+    fat = (bmi>25.0);
+    thin = (bmi<=18.5);
+    normal = (!fat) && (!thin);
+    
+    printf("BMI=%4.1f FAT:%d NORMANL:%d THIN:%d\n", bmi, fat, normal, thin);
+    
+    if(fat) printf("You are fat\n");
+    else if(thin) printf("You are thin\n");
+    else printf("You are normal\n");	
+    
+
     return 0;
 }
